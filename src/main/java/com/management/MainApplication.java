32df0fba31d9899cbf5b1e7b79c9f8a3e1e92ec7
Moncef khafif <96123439+Immortalxking@users.noreply.github.com>
2023-04-08 09:48:47 +0000

    co

--- src/main/java/com/management/MainApplication.java
+++ src/main/java/com/management/MainApplication.java
@@ -1,5 +1,6 @@
 package com.management;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,7 @@ public class MainApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(MainApplication.class, args);
+
+
     }
 }
---
+++ src/main/java/com/management/entities/Car.java
@@ -0,0 +1,80 @@
+package com.management.entities;
+
+import jakarta.persistence.*;
+import org.jetbrains.annotations.NotNull;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+@Entity
+@Table(name="car")
+public class Car {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String make;
+
+
+    private String model;
+
+    @NotNull
+
+    private Integer year;
+
+    @NotNull
+
+    private Double price;
+
+    public Car() {
+    }
+
+    public Car(String make, String model, Integer year, Double price) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
